0242f097995a8a65548349354d79ca7d5edd68e7
Mattia <145142275+12Jimbo@users.noreply.github.com>
2025-04-03 13:00:45 +0200

    Create text_exploration.py

---
+++ text_processing/text_exploration.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def initials(s):
+    # Given a Series of strings, it retruns all unique first characters
+    s = s.map(lambda x: x[0] if isinstance(x, str) else x)
+    return s.value_counts(dropna = False)
+
+def finals(s):
+    # Given a Series of strings, it retruns all unique first characters
+    s = s.map(lambda x: x[-1] if isinstance(x, str) else x)
+    return s.value_counts(dropna = False)
