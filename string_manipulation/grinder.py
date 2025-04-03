1a441c68371116c809df5aa32fb6c4603346b21f
Mattia <145142275+12Jimbo@users.noreply.github.com>
2025-04-03 12:28:35 +0200

    Create grinder.py
    Processes strings to leave only words meaningful  to a content analysis.

---
+++ string_manipulation/grinder.py
@@ -0,0 +1,30 @@
+import char_cleaning
+import grammar_elements
+
+def words_grinder(strings, clean_punct = True, garbage_list = None):
+  # This function will take a Pandas Series of dated strings,
+  # and return a long DataFrame of single upper case words;
+  # it will eliminate punctuation, spaces,
+  # auxiliary verbs, pronouns, and articles
+
+  # Converting all strings to upper case
+  strings = strings.str.upper()
+
+  # Eliminating contracted negations
+  strings = strings.str.replace("N\'T", '')
+
+  # Eliminating contracted possesives
+  strings = strings.str.replace("\'S", '')
+
+  # Cleaning punctuation
+  if clean_punct == True:
+    strings = clean(strings, punctuations, is_a_word = False, replace_with = '')
+
+  # Trimming empty trailing or leading spaces
+  strings = strings.str.strip()
+
+  # Cleaning all words in garbage_list from strings
+  for words_list in garbage_list:
+    strings = clean(strings, words_list, make_it_upper = True)
+
+  return strings
