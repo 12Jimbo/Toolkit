a24486aba037271c5453de3a0025de59c0dc9515
Mattia <145142275+12Jimbo@users.noreply.github.com>
2025-04-03 12:58:39 +0200

    Create data_types.py
    Stuff you might want to know about how data types are distributed in your pandas data frame

---
+++ data_exploration/data_types.py
@@ -0,0 +1,37 @@
+import pandas as pd
+  
+def col_types(df):
+# prints out the number of element types found in each column of a given df
+  for i in df.columns:
+        print(
+            df[i].map(type).value_counts(),
+            '\n NaN values: ',
+            df[i].isna().sum(),
+            '\n'
+    )
+
+
+def nan2nan(df, col_1_name, col_2_name):
+    # Given 2 columns in a data frame df, this function
+    # returns a list with the number of NaNs in the first,
+    # The number of NaNs in the second,
+    # And the number fo rows where both cols have NaN value
+    
+    col_1 = col_1_name 
+    col_2 = col_2_name
+    
+    # Number of NaN rows in col_1:    
+    nans_1 = pd.isna(df[col_1]).sum()
+    # Number of NaN rows in col_2:
+    nans_2 = pd.isna(df[col_2]).sum()
+    # Number of rows where both col_1 and col_2 are NaN:
+    nans_1and2 = (pd.isna(df[col_1]) * pd.isna(df[col_2])).sum()
+    
+    print(
+    'NaN values in {}: '.format(col_1),
+    nans_1,
+    '\n\nNaN values in {}: '.format(col_2),
+    nans_2,
+    '\n\nNaN values in the same row for both {} and {}: '.format(col_1, col_2),
+    nans_1and2
+)
