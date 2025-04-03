e4554978d8901e877ae9c18d71b48e872f1649df
Mattia <145142275+12Jimbo@users.noreply.github.com>
2025-04-03 12:14:58 +0200

    Create grammar_elements.py
    A list of the most common words and punctuations in English, categorized.

---
+++ grammar_elements.py
@@ -0,0 +1,51 @@
+# Defining here various lists of grammar elements
+punctuations = ['.', ',', ';', ':', '!', '?', '-', '_', '(', ')', '[', ']', '{', '}', '"', "'", '...', '—', '–', '‽', '@', '#', '$', '%', '^', '&', '*', '`', '~', '/', '\\', '|', '<', '>']
+
+articles = ['a', 'an', 'the']
+
+conjunctions = [
+  'and', 'but', 'or', 'nor', 'for', 'so', 'yet', 'of',
+  'although', 'because', 'since', 'unless', 'while', 'if', 'when', 'until', 'before', 'after',
+  'both', 'either', 'neither', 'nor', 'whether', 'not only', 'but also', 'just as', 'so', 'as well as',
+  'as','than'
+  ]
+
+prepositions = [
+    "of", "in", "on", "at", "by", "with", "for", "from", "to", "about",
+    "against", "between", "during", "before", "after", "under", "over", "above", "below", "around",
+    "through", "across", "along", "behind", "beside", "near", "among", "upon", "toward", "between"
+]
+
+pronouns = [
+  'I', 'you', 'he', 'she', 'it', 'we', 'they',
+  'me', 'you', 'him', 'her', 'it', 'us', 'them',
+  'my', 'your', 'his', 'her', 'its', 'our', 'their',
+  'mine', 'yours', 'his', 'hers', 'its', 'ours', 'theirs',
+  'myself', 'yourself', 'himself', 'herself', 'itself', 'ourselves', 'yourselves', 'themselves',
+  'this', 'that', 'these', 'those',
+  'someone', 'anyone', 'everyone', 'no one', 'somebody', 'anybody', 'everybody', 'nobody',
+  'something', 'anything', 'everything', 'nothing', 'each', 'few', 'many', 'none', 'all', 'some',
+  'who', 'whom', 'whose', 'which', 'what',
+  'who', 'whom', 'whose', 'which', 'that'
+  ]
+
+prefixes = ['jr','ms','sr','miss']
+
+auxiliary_verbs = [
+  'to',
+  'am', 'is', 'are', 'was', 'were', 'been', 'being', 'be',
+  'have', 'has', 'had', 'having',
+  'do', 'does', 'did', 'done', 'doing',
+  'can', 'could',
+  'will', 'would',
+  'shall', 'should',
+  'may', 'might',
+  'must',
+  'ought',
+  'dare', 'dared',
+  'need', 'needed',
+  'used to',
+  'be going to'
+  ]
+
+adverbs = ['not', 'where', 'when']
